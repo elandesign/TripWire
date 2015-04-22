15707ba763ac100f5346228d21c26bc8cd622f35
Paul Smith <paul@freeagent.com>
2015-04-22 15:19:35 +0100

    First pass at code

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 ALTERNATE_CORE   = sparkfun
 BOARD_TAG        = promicro16
+USER_LIB_PATH    = $(realpath libraries)
+ARDUINO_LIBS     = Adafruit_GFX Adafruit_PCD8544 Timer SPI
 
 ifndef ARDMK_DIR
 	ARDMK_DIR = $(realpath libraries/Arduino-Makefile)
--- TripWire.ino
+++ TripWire.ino
@@ -1,9 +1,61 @@
+#define SENSOR_PIN 2 // Needs to be a pin with an interrupt
+#define SENSOR_INTERRUPT 1
+
+#define LCD_DC 10
+#define LCD_CS 18
+#define LCD_RST 19
+#define LCD_BACKLIGHT 20
+#define LCD_TIMEOUT 1000
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include <Timer.h>
+
+Timer timer;
+Adafruit_PCD8544 display = Adafruit_PCD8544(LCD_DC, LCD_CS, LCD_RST);
+
+int count = 0;
+
+void dimDisplay()
+{
+  digitalWrite(LCD_BACKLIGHT, LOW);
+}
+
+void updateDisplay()
+{
+  display.clearDisplay();
+  digitalWrite(LCD_BACKLIGHT, HIGH);
+  display.setCursor(0,0);
+  display.println(count);
+  timer.after(LCD_TIMEOUT, dimDisplay);
+}
+
+void incrementCounter()
+{
+  count++;
+  updateDisplay();
+}
+
+void setupDisplay()
+{
+  display.begin();
+  display.setContrast(50);
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.clearDisplay();
+  updateDisplay();
+}
+
 void setup()
 {
-
+  pinMode(LCD_BACKLIGHT, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
+  setupDisplay();
+  attachInterrupt(SENSOR_INTERRUPT, incrementCounter, RISING);
 }
 
 void loop()
 {
-
+  timer.update();
 }
